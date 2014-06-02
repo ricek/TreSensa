ff32a10959f15152b6f423d664ecbaecece820d1
GanixiL <xiangli1997@gmail.com>
2014-06-02 17:04:40 -0400

    Collision Test 3
    it seems the bound of 2 objects are their first frame in spritesheet,
    not the last frame.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,15 +5,13 @@ GameScreen = function(width,height)
     GameScreen.superclass.constructor.apply(this,arguments);
     this.backgroundColor = "#CD9B00";
     this.blocks = new Array();
+    
     this.createBlock(300,300,"b1");
     this.createBlock(350,300,"b1");
-    this.createBlock(400,300,"b1");
-    this.createBlock(450,300,"b1");
-    this.createBlock(500,300,"b1");
-    this.createBlock(550,300,"b1");
-    /*for(var x =0;x<this.blocks.length-1;x++){
-        this.blocks[x].addEventListener("update",this.checkCollision.bind(this.blocks[x]));
-    }  */  
+    this.checkCollision();
+    
+    
+
     
 };
 
@@ -24,6 +22,8 @@ GameScreen.prototype =
     updateBrick: function(event){
         var b = event.currentTarget;
         b.play();
+        
+
     },
     createBlock: function(posx,posy,type){
         var b =new TGE.SpriteSheetAnimation().setup(
@@ -43,21 +43,19 @@ GameScreen.prototype =
         this.addChild(b);
         this.blocks[this.blocks.length-1].addEventListener("mousedown",this.updateBrick.bind(this.blocks[this.blocks.length-1]));
         
-        
-       
 
     },
     getBounds:function(){
         return this._checkVisibilityChange(),this._mBoundingInfoDirty&&this._updateAABB(),this._mAABB
     },
-    collision: function(b1,b2){
-       if(b1.getBounds().intersects(b2.getBounds(),0.5,0.5)){
+    /*collision: function(b1,b2){
+       	if(b1.getBounds().intersects(b2.getBounds()),0.5,0.5){
             return true;
         }
         else{
             return false;
         }
-    },
+    },*/
    
 
     /*updateTeemo:function(event){
@@ -69,11 +67,14 @@ GameScreen.prototype =
     },*/
     checkCollision:function(){
         for(var x =0;x<this.blocks.length-1;x++){
-            if(this.collision(this.blocks[x],this.blocks[x+1]))
+        	
+            if(this.blocks[x].getBounds().intersects(this.blocks[x+1].getBounds()),0.5,0.5)
+            	
                 this.blocks[x+1].play();
         }
         
     },
+
 
 };
 
